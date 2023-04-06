94343bd913e5531161a70e569b0d3f88f9901683
zoundzEc <andersonmikol@live.com>
2023-04-05 19:48:24 -0500

    Class Control history

--- giffApp/src/app/gifs/busqueda/busqueda.component.ts
+++ giffApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -20,6 +20,9 @@ export class BusquedaComponent {
 
   buscar(){
     const valor = this.txtBuscar.nativeElement.value;
+    if(valor.trim().length === 0){
+      return;
+    }
     this.gifServices.buscarGifs(valor);
     this.txtBuscar.nativeElement.value = "";
   }
