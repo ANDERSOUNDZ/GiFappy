a83fcfb1442e8c5997ffab94cf0dc3e96ba7f3d7
Anderson Chanchay <andersonmikol@live.com>
2023-04-24 20:09:54 -0500

    Ejemplo viewchild2023

--- giffApp/src/app/gifs/components/search-box/search-box.component.ts
+++ giffApp/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   template: `
   <h5>Buscar:</h5>
-  <input type="text" class="form-control" placeholder="Buscar gifs...">
+  <input type="text" class="form-control" placeholder="Buscar gifs..." (keyup.enter)="searchtag()" #txtTagInput>
   `
 })
 
 export class SearchBoxComponent {
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
   constructor() { }
+
+  searchtag(){
+    const newTag = this.tagInput.nativeElement.value;
+  }
 
 }
