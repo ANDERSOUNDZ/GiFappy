6db1672be8d4050b99a2a004f913fe90f0036ee8
Anderson Chanchay <andersonmikol@live.com>
2023-04-24 22:45:06 -0500

    Logica de peticiones http

--- giffApp/src/app/gifs/services/gifs.service.ts
+++ giffApp/src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,18 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
-  private _tagHistory: string[] = [];
+  //Almacenar los gifs
+  public gifList: Gif[] = [];
 
-  constructor() { }
+  private _tagHistory: string[] = [];
+  private apiKey: string = '60Si5mOEPfUo9wVQ32kuZAYoOdx4wdKe';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) { }
 
   get tagHistory(){
     return [...this._tagHistory];
@@ -23,7 +30,18 @@ export class GifsService {
   searchTag(tag: string):void {
     if(tag.length == 0) return;
     this.organizeHistory(tag);
-    console.log(this._tagHistory);
+    //Creo los parametros para los valores de el api, es por buenas practicas de programacion
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag)
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search?`,{params})
+    .subscribe(resp =>{
+      this.gifList = resp.data;
+      console.log({gifs: this.gifList});
+    })
+    ;
   }
 
 }
